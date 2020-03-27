fa084abbacba34594f5b68358c5c14523255e622
M. Baumann <19737739+artofbeinghuman@users.noreply.github.com>
2020-03-27 17:36:08 +0100

    added zoomAndCenter getter for Cluster

--- lib/src/cluster.dart
+++ lib/src/cluster.dart
@@ -25,6 +25,17 @@ class Cluster<T> {
         "_$count";
   }
 
+  /// returns zoom level and Center LatLng location, such that GoogleMaps animated
+  /// to that position will show all ClusterItems contained in this Cluster.
+  List<dynamic> get zoomAndCenter {
+    double zoom = 12.3;
+    LatLng center = LatLng(48.141988471740895, 11.577429659664631);
+
+    // TODO: find bounding box of clusteritems and calculate zoom level and center position.
+    // Could tihs be done early and saved as a property of this cluster?
+    return [zoom, center];
+  }
+
   @override
   String toString() {
     return 'Cluster of $count $T (${location.latitude}, ${location.longitude})';
